9ada917d17b2276d5c76dda91a787a33384f4d27
adubcatc <austinrwilkins@gmail.com>
2023-02-12 15:44:49 -0600

    initial commit

---
+++ src/views/page/404.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="card">
+		<div class="card-content">
+			<span class="card-title">404 – Page Not Found</span>
+			<p>
+				OPE! Matt made a problem dont ya know well tell your folks I says hi!.
+			</p>
+			<p>
+				We're sorry, but the content you request could not be found. If you believe that you are receiving this message in error, please email the webmaster. In the meantime, I have provided a F14 to ease your sorrows.
+			</p>
+			<p>
+				<img src="https://cdn.discordapp.com/attachments/1058781321381560421/1058781413626871808/f14_sorrow.webp">
+			</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+.card-content {
+	p + p {
+		margin-top: 1.5em;
+	}
+}
+img {
+	max-width: 100%;
+}
+</style>
---
+++ src/views/page/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="card home_intro">
+      <div class="card-content">
+        <span class="card-title">Welcome to Fort Worth ARTCC</span>
+        <p>
+          The Fort Worth ARTCC covers parts of Texas, Oklahoma, New Mexico, Louisiana, and Arkansas, with a square mileage of over 147,000. Our goal and mission is to provide professional air traffic control services, foster a welcoming community, and enjoy this hobby to it's greatest extent possible. Students get a unique experience here with our student:instructor assignment program and get the advantage of consistent instruction and scheduling once assigned an instructor. We hope to see you on the scopes soon!
+        </p>
+      <br>
+        <p>
+          “Texas is neither southern nor western. Texas is Texas.”
+        </p>
+        <div class="visit" v-if="!user.isLoggedIn || (!user.data.vis && !user.data.isMem)">
+          <p>
+            If you would like to become a visitor, please click the button below.
+          </p>
+          <router-link to="/controllers/visit" class="btn btn-waves">Become a Visitor</router-link>
+        </div>
+      </div>
+    </div>
+    <EventSummary />
+    <NewsSummary />
+    <WeatherInfo />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import WeatherInfo from '../partial/WeatherInfo.vue';
+import NewsSummary from '../partial/NewsSummary.vue';
+import EventSummary from '../partial/EventSummary.vue';
+
+export default {
+  name: 'Home',
+  title: 'Home',
+  components: {
+    NewsSummary,
+    WeatherInfo,
+    EventSummary
+  },
+  computed: {
+    ...mapState('user', [
+      'user'
+    ])
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.visit {
+  margin-top: 1em;
+  text-align: center;
+
+  p {
+    text-align: left;
+  }
+
+  .btn {
+    margin-top: 1em;
+  }
+}
+</style>
