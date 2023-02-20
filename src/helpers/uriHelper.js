bd89e1991462a425ae5cd5955ced1c99aeb69905
adubcatc <austinrwilkins@gmail.com>
2023-02-19 21:11:53 -0600

    update for vatsimconnect

--- src/helpers/uriHelper.js
+++ src/helpers/uriHelper.js
@@ -5,8 +5,8 @@ export const vatsimAuthRedirectUrl = `${getAuthEndpoint()}/oauth/authorize?clien
   export const discordRedirectUrl = `https://discord.com/api/oauth2/authorize?client_id=1060336105267929088&redirect_uri=https%3A%2F%2Fwww.zauartcc.org%2Fconnect%2Fdiscord&response_type=code&scope=identify`;
   
   function getClientId() {
-    const betaClientId = 1182;
-    const prodClientId = 1144;
+    const betaClientId = 1225;
+    const prodClientId = 1226;
     const devClientId = 466;
   
     if (window.location.origin.includes("staging")) {
