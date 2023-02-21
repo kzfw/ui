24207f1f3a998118c2830f61efb3228512d97175
bkinney272 <brandon.purchase@gmail.com>
2023-02-20 20:03:06 -0600

    Update academy.vue
    Added welcome

---
+++ src/views/dashboard/academy.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <div class="title-banner">
+        <h1>Welcome to the ZFW Training Academy</h1>
+      </div>
+      <!-- Other content goes here -->
+    </div>
+  </template>
+
+<style>
+.title-banner {
+  background-color: #002868;
+  padding: 20px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+.title-banner h1 {
+  font-size: 36px;
+  margin: 0;
+  color:#f2f2f2;
+}
+.title-banner p {
+  font-size: 18px;
+  margin: 0;
+}
+</style>
