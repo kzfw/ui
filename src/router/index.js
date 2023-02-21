6c25147293233d8d4fbb4c201363aeef9348fe17
bkinney272 <brandon.purchase@gmail.com>
2023-02-20 19:24:22 -0600

    More Updates
    Created new academy page

--- src/router/index.js
+++ src/router/index.js
@@ -272,7 +272,7 @@ const routes = [
 		path: '/acd', component: Dashboard, meta: { loggedIn: true }, children: [
 			{
 				path: '',
-				component: () => import('../views/dashboard/Index.vue')
+				component: () => import('../views/dashboard/academy.vue')
 			},
 			{
 				path: 'profile',
