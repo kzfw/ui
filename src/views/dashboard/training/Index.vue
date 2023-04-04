c1ba9c6c0b3c188198460b5bba1a4312289255ac
Daniel Duhon <danielgduhon@gmail.com>
2023-04-04 08:57:21 -0500

    Added card to dash/training, added router link to training/TrainingRequst.vue

--- src/views/dashboard/training/Index.vue
+++ src/views/dashboard/training/Index.vue
@@ -1,5 +1,56 @@
 <template>
 	<div class="card">
+		<div class="training-request">
+			<div class="row row_no_margin">
+				<div class="card-title col s8"><span class="card-title">Initial Training Request</span></div>
+				<div class="col s4"><router-link to="/dash/training/initial"><span class="btn new_event_button right">Request</span></router-link></div>
+			</div>
+		</div>
+		<div class="loading_container" v-if="!upcomingSessions">
+			<Spinner />
+		</div>
+
+		<p class="no_sessions" v-else-if="upcomingSessions && upcomingSessions.length === 0">You have not requested training. Please use the button above to request training. You must do this in order to begin your training at ZFW.</p>
+
+		<div class="session_wrapper" v-else>
+			<table class="session_list striped">
+				<thead class="session_list_head">
+					<tr>
+						<th>Milestone</th>
+						<th>Start Time</th>
+						<th>End Time</th>
+						<th>Instructor</th>
+						<th class='options'>Options</th>
+					</tr>
+				</thead>
+				<tbody class="session_list_row" v-if="upcomingSessions">
+					<tr v-for="session in upcomingSessions" :key="session._id">
+						<td>{{session.milestone.code + ' - ' + session.milestone.name}}</td>
+						<td>{{dtLong(session.startTime)}}</td>
+						<td>{{dtLong(session.endTime)}}</td>
+						<td>{{session.instructor ? (session.instructor.fname + ' ' + session.instructor.lname) : 'Unfulfilled'}}</td>
+						<td class="options">
+                            <a :href="`#modal_delete_${session._id}`" data-position="top" data-tooltip="Cancel Training Session"
+							class="tooltipped modal-trigger"><i class="material-icons red-text text-darken-2">cancel</i></a>
+                        </td>
+                        <div :id="`modal_delete_${session._id}`" class="modal modal_delete">
+                            <div class="modal-content">
+                                <h4>Cancel Training Session?</h4>
+                                <p>This will remove the training session.  <br>
+								<br>If you are cancelling a scheduled session within 24 hours, please advise the instructor or ta@zfwartcc.net via email also.<br><br>
+								Are you sure you wish to cancel?</p>
+                            </div>
+                            <div class="modal-footer">
+                                <a href="#!" @click="deleteSession(session._id)" class="btn waves-effect">Delete</a>
+                                <a href="#!" class="btn-flat waves-effect modal-close">Cancel</a>
+                            </div>
+                        </div>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<div class="card">
 		<div class="card-content">
 			<div class="row row_no_margin">
 				<div class="card-title col s8"><span class="card-title">Training Requests</span></div>
