0ec5ed416d67313261306434275bdac05bbfc560
bkinney272 <brandon.purchase@gmail.com>
2023-04-30 16:25:04 -0500

    fix
    fix3

--- src/router/index.js
+++ src/router/index.js
@@ -259,10 +259,6 @@ const routes = [
 				component: () => import('../views/dashboard/training/Index.vue')
 			},
 			{
-				path: 'training/instructorreq',
-				component: () => import('../views/dashboard/training/TrainingRequest.vue')
-			},
-			{
 				path: 'training/new',
 				component: () => import('../views/dashboard/training/Request.vue')
 			},
